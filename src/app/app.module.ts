c73146312fda49aa70f52540fdcef7243485c38f
Hima <geethun1247@gmail.com>
2020-04-27 22:28:00 +0530

    Http Example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { ArticleComponent } from './article/article.component';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+//For InMemory testing
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { TestData } from './test-data';
+import { StudentComponent } from './student/student.component';
+
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+      imports: [
+            BrowserModule,
+            HttpClientModule,
+            ReactiveFormsModule,
+            InMemoryWebApiModule.forRoot(TestData)
+      ],
+      declarations: [
+            AppComponent,
+            ArticleComponent,
+            StudentComponent
+      ],
+      providers: [],
+      bootstrap: [
+            AppComponent
+      ]
 })
-export class AppModule { }
+export class AppModule { } 
